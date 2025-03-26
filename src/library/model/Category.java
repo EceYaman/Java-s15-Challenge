e3ff274dd6c3a9abe1ee80390f9a334009524586
Ece Yaman <eceymn35@gmail.com>
2025-03-27 01:04:54 +0300

    Added Category class

--- src/library/model/Category.java
+++ src/library/model/Category.java
@@ -1,4 +1,21 @@
 package library.model;
 
 public class Category {
+    private CategoryType categoryType;
+
+    public Category(CategoryType categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public CategoryType getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(CategoryType categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public String getCategoryName() {
+        return categoryType.getDescription();
+    }
 }
