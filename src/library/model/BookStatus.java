66d94ee753b99d461964986d3472f9dc500df891
Ece Yaman <eceymn35@gmail.com>
2025-03-27 00:21:18 +0300

    Added BookStatus enum

---
+++ src/library/model/BookStatus.java
@@ -0,0 +1,16 @@
+package library.model;
+
+public enum BookStatus {
+    AVAILABLE,
+    BORROWED,
+    RESERVED;
+
+    public String getDescription() {
+        switch (this) {
+            case AVAILABLE: return "Kitap mevcut ve ödünç verilebilir.";
+            case BORROWED: return "Kitap ödünç alınmış.";
+            case RESERVED: return "Kitap rezerve edilmiş.";
+            default: return "Bilinmeyen durum.";
+        }
+    }
+}
