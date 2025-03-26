dc7fba91d9a0e1722db211d112556278da530ba9
Ece Yaman <eceymn35@gmail.com>
2025-03-27 00:49:43 +0300

    Added Book class with properties, constructor, and methods

--- src/library/model/Book.java
+++ src/library/model/Book.java
@@ -1,4 +1,57 @@
 package library.model;
 
 public class Book {
+    private int id;
+    private String title;
+    private Author author;
+    private Category category;
+    private BookStatus status;
+
+    public Book(int id, String title, Author author, Category category, BookStatus status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
 }
