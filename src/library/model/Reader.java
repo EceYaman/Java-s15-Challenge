534c55c3125c9a654ebf8b09e8fd96fafd8afb1e
Ece Yaman <eceymn35@gmail.com>
2025-03-27 15:49:41 +0300

    Created Reader class

--- src/library/model/Reader.java
+++ src/library/model/Reader.java
@@ -1,4 +1,35 @@
 package library.model;
 
-public class Reader {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader extends Person {
+    private List<Book> borrowedBooks;
+    private int borrowLimit = 5;
+
+    public Reader(int id, String name, String email) {
+        super(id, name, email);
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public void addBorrowedBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void removeBorrowedBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Reader [id=" + getId() + ", name=" + getName() + ", email=" + getEmail() + "]");
+    }
 }
