1b378abd77efcd1d971432a38c8e343add89a8f9
Ece Yaman <eceymn35@gmail.com>
2025-03-27 15:58:00 +0300

    Created Librarian class

--- src/library/model/Librarian.java
+++ src/library/model/Librarian.java
@@ -1,4 +1,24 @@
 package library.model;
 
-public class Librarian {
+public class Librarian extends Person{
+    private String staffNumber;
+
+    public Librarian(int id, String name, String email, String staffNumber) {
+        super(id, name, email);
+        this.staffNumber = staffNumber;
+    }
+
+    public String getStaffNumber() {
+        return staffNumber;
+    }
+
+    public void setStaffNumber(String staffNumber) {
+        this.staffNumber = staffNumber;
+    }
+
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Librarian [id=" + getId() + ", name=" + getName() + ", staffNumber=" + staffNumber + "]");
+    }
 }
