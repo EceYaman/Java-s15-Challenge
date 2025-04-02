5110cc0b20d6919f414906e1b0d95640d28cbcf2
Ece Yaman <eceymn35@gmail.com>
2025-04-02 13:39:51 +0300

    Added IdGenerator for unique IDs

--- src/com/library/util/IdGenerator.java
+++ src/com/library/util/IdGenerator.java
@@ -1,4 +1,14 @@
 package com.library.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IdGenerator {
+    private static Map<String, Integer> counters = new HashMap<>();
+
+    public static int generateId(String key) {
+        int id = counters.getOrDefault(key, 0) + 1;
+        counters.put(key, id);
+        return id;
+    }
 }
